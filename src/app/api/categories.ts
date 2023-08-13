314df46d73943ddd16ac40850d60fea428fddffd
dmitriy3305 <dmitrijkrymskij6@gmail.com>
2023-08-13 14:04:59 +0400

    fix: categories.ts

--- src/app/api/categories.ts
+++ src/app/api/categories.ts
@@ -3,11 +3,6 @@ import ctpClient from './buildClient';
 
 const apiRoot = createApiBuilderFromCtpClient(ctpClient).withProjectKey({ projectKey: 'ecommerce-application2023q1' });
 const getCategories = async () => {
-  console.log(apiRoot.apiClients);
-  console.log(apiRoot.customers);
-  console.log(apiRoot.apiClients);
-  console.log(apiRoot.apiClients);
-
   return apiRoot.categories().get().execute();
 };
 getCategories()
