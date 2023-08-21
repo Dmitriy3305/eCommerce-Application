2f52571e9eb487ec22ce773b45297c08f43bda27
Your Name <veronichka.danilina@mail.ru>
2023-08-21 22:13:47 +0400

    fix: delete console.log

--- src/app/view/home-page/slider.ts
+++ src/app/view/home-page/slider.ts
@@ -85,7 +85,6 @@ export default class Slider extends DOMComponent<HTMLElement> {
     const cardsProduct = [];
     const productsRepository = new ProductsRepository();
     productsRepository.getProducts().then((response) => {
-      console.log(response);
       for (let i = 0; i < response.length; i += 1) {
         const { images } = response[i].masterData.current.masterVariant;
         const { name } = response[i].masterData.current;
