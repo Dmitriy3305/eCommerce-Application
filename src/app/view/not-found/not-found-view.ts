c591a75ab5d045c7b4959d86218c2365419bb954
Quaqva <sanek15056@gmail.com>
2023-08-21 22:17:41 +0400

    feat: add not found view

---
+++ src/app/view/not-found/not-found-view.ts
@@ -0,0 +1,40 @@
+import DOMComponent, { ElementParameters } from '../../../components/base-component';
+import { Tags } from '../../../types/dom-types/enums';
+import createLink from '../../../utils/create-link';
+import AppRouter from '../../router/router';
+import { AppLink } from '../../router/router-types';
+import AppView from '../view';
+import layout from './not-found.html';
+
+enum NotFoundCssClasses {
+  Main = 'not-found',
+  HomeButton = 'not-found__go-back',
+}
+
+export default class NotFoundView extends AppView {
+  private static MAIN_PARAMS: ElementParameters = {
+    tag: Tags.Main,
+    classList: [NotFoundCssClasses.Main],
+  };
+
+  public constructor(router: AppRouter) {
+    super(router);
+  }
+
+  protected override createMain(): DOMComponent<HTMLElement> {
+    const main = new DOMComponent<HTMLElement>(NotFoundView.MAIN_PARAMS);
+    const contentWrapper = DOMComponent.FromHTML(layout);
+    const returnToHomeButton = createLink(
+      {
+        tag: Tags.Button,
+        classList: [NotFoundCssClasses.HomeButton],
+        textContent: 'Return to home',
+      },
+      this.router,
+      AppLink.Main
+    );
+    contentWrapper.append(returnToHomeButton);
+    main.append(contentWrapper);
+    return main;
+  }
+}
